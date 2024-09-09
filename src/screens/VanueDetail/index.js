decdb2575679a7ff80b0802520083c5a85daee21
Chaman Kha <chaman.devv@gmail.com>
2024-09-09 22:57:03 +0500

    ewein

--- src/screens/VanueDetail/index.js
+++ src/screens/VanueDetail/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {Family} from '../../components/family';
 const VanueDetail = ({navigation, route}) => {
   const item = route.params.item;
   return (
@@ -8,7 +9,37 @@ const VanueDetail = ({navigation, route}) => {
         source={item.img}
         style={{width: '100%', height: 250, borderRadius: 20}}
       />
+      <Text style={styles.name}>{item.name}</Text>
+      <View style={styles.line} />
+      <Text style={styles.heading}>
+        Availability:
+        <View
+          style={{height: 1, backgroundColor: 'lightgrey', width: '100%'}}
+        />
+      </Text>
+      <Text style={{fontFamily: Family}}>{item.availability}</Text>
+      <View style={styles.line} />
     </View>
   );
 };
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 30,
+    fontWeight: '700',
+    fontFamily: Family,
+    marginTop: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontFamily: Family,
+    fontWeight: '500',
+    marginBottom: 10,
+  },
+  line: {
+    height: 1,
+    backgroundColor: 'lightgrey',
+    width: '100%',
+    marginVertical: 10,
+  },
+});
 export default VanueDetail;
